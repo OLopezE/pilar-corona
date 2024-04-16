63b1ea543fe5859808b39ca47f7f85f0652e4d69
Oscar francisco Lopez escobar <olopeze@outlook.com>
2024-04-15 22:11:18 -0600

    Adding colors to curriculum

--- src/js/views/Curriculum/Curriculum.jsx
+++ src/js/views/Curriculum/Curriculum.jsx
@@ -1,58 +1,66 @@
 import React from 'react';
+import {
+  SiAdobephotoshop,
+  SiSketchup,
+  SiBlender
+} from "react-icons/si";
 
 import pili from '/assets/images/pili.png'
 
-import { CurriculumSection } from './Curriculum.style';
+import {
+  CurriculumSection,
+  SoftwareList
+} from './Curriculum.style';
 
 const Curriculum = () => {
   return (
   <CurriculumSection>
+    <h1>CURRICULUM</h1>
     <div>
-      <h1>CURRICULUM</h1>
-      <h2>PILAR CORONA - ARCHITECTURE STUDENT</h2>
-      <h3>EDUCATION</h3>
-      <ul>
-        <li>ISAD Escuela de Arquitectura y Diseño Arquitectura, 2017- 2018</li>
-        <li>Isthmus Norte Escuela de Arquitectura Arquitectura, 2020 - current</li>
-      </ul>
-      <h3>LANGUAGES</h3>
-      <ul>
-        <li>Spanish - Native</li>
-        <li>English - Advanced</li>
-        <li>German - Goeth - Zertifikat A2. </li>
-        <li>Mandarin - UBC Extended Learning Beginner 1.</li>
-      </ul>
-      <h3>SOFTWARE</h3>
-      <ul>
-        <li>Sketchup</li>
-        <li>Photoshop</li>
-        <li>Vray</li>
-        <li>Enscape</li>
-        <li>Autocad</li>
-        <li>Blender</li>
-      </ul>
-      <h3>INTERESTS AND SKILLS</h3>
-      <ul>
-        <li>Diseño gráfico</li>
-        <li>Modelado 3D</li>
-        <li>Ventas</li>
-        <li>Fotografía</li>
-        <li>Estudio y análisis bioclimático</li>
-        <li>Diseño arquitectónico</li>
-        <li>Dibujo</li>
-        <li>Diseño de mobiliario</li>
-        <li>Construcción</li>
-        <li>Idiomas</li>
-      </ul>
-      <img src={pili} />
-      <p>
-        Soy una persona proactiva, excelente
-        compañera de equipo, organizada y
-        responsable. Mi objetivo es un
-        puesto desafiante y dinámico dónde poder sumar experiencia y nuevos
-        conocimientos.
-      </p>
-
+      <div>
+        <h2>PILAR CORONA - ARCHITECTURE STUDENT</h2>
+        <h3>EDUCATION</h3>
+        <ul>
+          <li>ISAD Escuela de Arquitectura y Diseño Arquitectura, 2017- 2018</li>
+          <li>Isthmus Norte Escuela de Arquitectura Arquitectura, 2020 - current</li>
+        </ul>
+        <h3>LANGUAGES</h3>
+        <ul>
+          <li>Spanish - Native</li>
+          <li>English - Advanced</li>
+          <li>German - Goeth - Zertifikat A2. </li>
+          <li>Mandarin - UBC Extended Learning Beginner 1.</li>
+        </ul>
+        <h3>SOFTWARE</h3>
+        <SoftwareList>
+          <SiSketchup />
+          <SiAdobephotoshop />
+          <SiBlender />
+        </SoftwareList>
+        <h3>INTERESTS AND SKILLS</h3>
+        <ul>
+          <li>Diseño gráfico</li>
+          <li>Modelado 3D</li>
+          <li>Ventas</li>
+          <li>Fotografía</li>
+          <li>Estudio y análisis bioclimático</li>
+          <li>Diseño arquitectónico</li>
+          <li>Dibujo</li>
+          <li>Diseño de mobiliario</li>
+          <li>Construcción</li>
+          <li>Idiomas</li>
+        </ul>
+      </div>
+      <div>
+        <img src={pili} />
+        <p>
+          Soy una persona proactiva, excelente
+          compañera de equipo, organizada y
+          responsable. Mi objetivo es un
+          puesto desafiante y dinámico dónde poder sumar experiencia y nuevos
+          conocimientos.
+        </p>
+      </div>
     </div>
   </CurriculumSection>
   )
--- src/js/views/Curriculum/Curriculum.style.js
+++ src/js/views/Curriculum/Curriculum.style.js
@@ -5,13 +5,42 @@ const CurriculumSection = styled.section`
   padding: 1rem;
   padding-top: 4rem;
   display: grid;
+  justify-content: center;
 
   > div {
     max-width: 1200px;
     justify-self: center;
   }
+
+  @media (min-width: 980px) {
+    > div {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      > div {
+      }
+    }
+
+  }
+`;
+
+const SoftwareList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 1rem;
+
+  > svg {
+    width: 100%;
+    height: 100%;
+    color: #ea9cf4;
+    transition: all 0.4s;
+
+    &:hover {
+      color: #17fc2a;
+    }
+  }
 `;
 
 export {
-  CurriculumSection
+  CurriculumSection,
+  SoftwareList
 }
